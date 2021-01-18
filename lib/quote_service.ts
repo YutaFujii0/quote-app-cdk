a3e412f14db26885324999a30420166c87b60086
YutaFujii0 <y.f.mar2@gmail.com>
2021-01-18 09:41:22 +0900

    Add New record of route53

--- lib/quote_service.ts
+++ lib/quote_service.ts
@@ -3,6 +3,10 @@ import * as apigatewayv2 from "@aws-cdk/aws-apigatewayv2";
 import * as integrations from "@aws-cdk/aws-apigatewayv2-integrations";
 import * as lambda from "@aws-cdk/aws-lambda";
 import * as iam from "@aws-cdk/aws-iam";
+import { Certificate }  from "@aws-cdk/aws-certificatemanager";
+// import * as route53 from "@aws-cdk/aws-route53";
+import { HostedZone, ARecord, RecordTarget } from "@aws-cdk/aws-route53";
+import * as alias from "@aws-cdk/aws-route53-targets";
 // import * as s3 from "@aws-cdk/aws-s3";
 // import { HttpMethod } from "@aws-cdk/aws-apigatewayv2";
 
@@ -11,6 +15,7 @@ export class QuoteService extends core.Construct {
     super(scope, id);
 
     const tableName = 'quote-service-dev1';
+    const domainName = 'quote-service-cdk.yutafujii.net';
 
     const lambdaRole = new iam.Role(this, 'ServiceLambda', {
       assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
@@ -94,5 +99,38 @@ export class QuoteService extends core.Construct {
       methods: [ apigatewayv2.HttpMethod.GET ],
       integration: seedIntegration,
     })
+
+    // Create Route53 - API Gateway Custom Domain - API mapping - lambda
+    const certificate = Certificate.fromCertificateArn(
+      this,
+      'string',
+      process.env.ACM_CERTIFICATE || ''
+    );
+
+    const customDomain = new apigatewayv2.DomainName(this, domainName, {
+      certificate: certificate,
+      domainName: domainName,
+    });
+
+    const map = new apigatewayv2.HttpApiMapping(this, 'HttpMapping', {
+      api: api,
+      domainName: customDomain,
+      stage: api.defaultStage,
+    })
+
+    const hostedZone = HostedZone.fromHostedZoneAttributes(
+      this,
+      'HostedZone',
+      {
+        hostedZoneId: process.env.HOSTED_ZONE_ID || '',
+        zoneName: 'yutafujii.net',
+      },
+    )
+
+    new ARecord(this, 'ARecord', {
+      zone: hostedZone,
+      recordName: 'quote-service-cdk',
+      target: RecordTarget.fromAlias(new alias.ApiGatewayv2Domain(customDomain))
+    });
   }
 }
